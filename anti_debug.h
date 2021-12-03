9f21bec1ccf06f7a5fba2a464b8a3b1c429bc272
장효원 <>
2021-12-03 17:06:41 +0900

    안티 디버깅 추가

---
+++ anti_debug.h
@@ -0,0 +1,15 @@
+//
+//  anti_debug.h
+//  HAVETODO
+//
+//  Created by 장효원 on 2021/12/03.
+//
+
+#ifndef anti_debug_h
+#define anti_debug_h
+
+#include <stdio.h>
+
+void m6d64657663616d70(void) __attribute__((always_inline));
+
+#endif
---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  HAVETODO
+//
+//  Created by 장효원 on 2021/12/03.
+//
+
+import UIKit
+
+m6d64657663616d70() // Launching it before everything else
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
