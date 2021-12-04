92219b273f1f30b41b9047bb63a332bb8ca2571f
장효원 <>
2021-12-04 23:57:41 +0900

    안티 디버깅 해제

---
+++ main_temp.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  HAVETODO
+//
+//  Created by 장효원 on 2021/12/03.
+//
+
+import UIKit
+
+
+//만약 안티 디버깅 사용한다면 주석 해제 (main_temp를 main으로 변경해줘야함)
+/*
+m6d64657663616d70() // Launching it before everything else
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
+*/
