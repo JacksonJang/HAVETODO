b3d15220a4f753917794e5eefc04721d86e29eff
장효원 <>
2021-12-05 18:32:53 +0900

    상수 추가

---
+++ HAVETODO/Common/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  HAVETODO
+//
+//  Created by 장효원 on 2021/12/05.
+//
+
+import UIKit
+
+let server_url = "http://janghyo.com"
+
+//크기
+let WIDTH = UIScreen.main.bounds.width
+let HEIGH = UIScreen.main.bounds.height
