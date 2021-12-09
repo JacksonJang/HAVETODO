a9a15a1061165041fb0e55d923fa420afc9cef56
장효원 <>
2021-12-09 17:21:36 +0900

    UITableView Drag & Drop 적용

---
+++ HAVETODO/ViewController/Memo/MemoListCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoListCell.swift
+//  HAVETODO
+//
+//  Created by 장효원 on 2021/12/09.
+//
+
+import UIKit
+
+class MemoListCell : UITableViewCell{
+    @IBOutlet var titleLabel: UILabel!
+    
+}
--- HAVETODO/ViewController/Memo/MemoListVC.swift
+++ HAVETODO/ViewController/Memo/MemoListVC.swift
@@ -5,14 +5,97 @@
 //  Created by 장효원 on 2021/12/05.
 //
 
+/*
+ 
+ UITableView Editing Mode 관련 내용
+ https://furang-note.tistory.com/11
+ 
+ UITableView Drag & Drop으로 Row 이동
+ https://furang-note.tistory.com/31
+ 
+ */
+
 import UIKit
 
 class MemoListVC: BaseViewController, Storyboarded {
     static var storyboardName: String = String(describing: MemoListVC.self)
     
+    @IBOutlet var tableView: UITableView!
+    
+    var data:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.dragDelegate = self
+        tableView.dropDelegate = self
+        tableView.dragInteractionEnabled = true
+        tableView.register(UINib(nibName: "MemoListCell", bundle: nil), forCellReuseIdentifier: "MemoListCell")
+        
+        
+        
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        var i = 0
+        while(i < 10){
+            self.data.append("test")
+            i += 1
+        }
+        
+        tableView.reloadData()
     }
 }
 
+extension MemoListVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemoListCell", for: indexPath) as! MemoListCell
+        
+        cell.titleLabel.text = data[indexPath.row] + String(indexPath.row)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    // Move Row Instance Method
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print("\(sourceIndexPath.row) -> \(destinationIndexPath.row)")
+        let moveCell = self.data[sourceIndexPath.row]
+        self.data.remove(at: sourceIndexPath.row)
+        self.data.insert(moveCell, at: destinationIndexPath.row)
+    }
+}
+
+// MARK:- UITableView UITableViewDragDelegate
+extension MemoListVC: UITableViewDragDelegate {
+func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return [UIDragItem(itemProvider: NSItemProvider())]
+    }
+}
+ 
+// MARK:- UITableView UITableViewDropDelegate
+extension MemoListVC: UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
+        if session.localDragSession != nil {
+            return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        return UITableViewDropProposal(operation: .cancel, intent: .unspecified)
+    }
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) { }
+}
